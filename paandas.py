8a3c0a87f897e977e96cec11c363a9eb9920e527
Ryhor-Karnatsevich <spamjamesmorgan@gmail.com>
2024-11-26 09:55:01 +0000

    qq

--- paandas.py
+++ paandas.py
@@ -2,4 +2,10 @@ import pandas as pd
 import numpy as numpy
 import matplotlib.pyplot as plt
 table = pd.read_csv('data.csv',index_col = 0)
+#duple = table.drop_duplicates(subset=['Name','Department']) deletes all the duplicates of subset collumns
+#print(table['Is_Remote'].value_counts(sort=True,normalize=True)) #gives us the count of all repeated values in the collumn,normalize gives us a proportion of total
+
+#print(table[table['Department'] == 'HR']['Salary'].mean()) gives as mean of all HR salaries
+#print(table.groupby('Department')['Salary'].mean()) #better version of previous code
+print(table.groupby(['Department','Position']).agg(max,min))
 print(table)
